89296769c83950039b024e179009d806b59e76a8
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-10-26 08:07:46 +0100

    Add assignment 5

--- week05/tw_credential_blank.R
+++ week05/tw_credential_blank.R
@@ -1,4 +1,4 @@
-api_key <- "***"
-api_secret <- "***"
-access_token <- "***"
-access_token_secret <- "***"
+api_key <- "cHaFKEG6cAzTeVhVE6pv9PAzs"
+api_secret <- "cO612Wl1z0vr6uxzQk43BrYyn4OhE2Cbn50uFBfGJl21pNUMIE"
+access_token <- "112961086-qrjlAsq5CNqkvkj1jBFf0fnPCjib73eskwLIKj0a"
+access_token_secret <- "DAolxsl9GjJsYMRnFRrea7pqubLLEG6c6BHShcxmC8FgU"
